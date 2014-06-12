40ba370b119376145738d554be966c9518910d88
Brian Romanko <brian@bigbigbomb.com>
2014-06-11 21:12:19 -0700

    Fixing start script

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ cd /tmp
 # try to remove the repo if it already exists
 rm -rf docker-test; true
 
-git clone <remote git repo>
+git clone https://github.com/bromanko/docker-test.git
 
-cd <git repo name>
+cd docker-test
 
 npm install
 
